65c24b2b11c63ba8860d2dac290246c19d776434
junsoo <choix712@umn.edu>
2023-11-10 19:55:37 +0900

    feat: 출력을 담당하는 OutputView 클래스 생성

---
+++ src/main/java/view/OutputView.java
@@ -0,0 +1,14 @@
+package view;
+
+import eums.OutputMessage;
+
+public class OutputView {
+
+    public static void printHelloMessage() {
+        System.out.println(OutputMessage.HELLO_MESSAGE);
+    }
+
+    public static void printAskMessage() {
+        System.out.println(OutputMessage.ASK_DATE);
+    }
+}
