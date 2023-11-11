6cd46b30f5dee302614ac1d8c3c0a4bf5fd64885
junsoo <choix712@umn.edu>
2023-11-11 16:58:07 +0900

    feat: 숫자들은 Numbers enum 클래스에서 관리

---
+++ src/main/java/enums/Numbers.java
@@ -0,0 +1,19 @@
+package enums;
+
+public enum Numbers {
+    MIN_DATE(1),
+    MAX_DATE(31),
+    MIN_MENU_COUNT(1),
+    MAX_MENU_COUNT(20),
+    ;
+
+    private final int numbers;
+
+    Numbers(int numbers) {
+        this.numbers = numbers;
+    }
+
+    public int getNumbers() {
+        return numbers;
+    }
+}
