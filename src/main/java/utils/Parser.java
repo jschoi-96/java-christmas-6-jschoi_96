18625d75ae83d58789c8d47368c9d4e8b0288d84
junsoo <choix712@umn.edu>
2023-11-11 17:33:14 +0900

    feat: 메뉴와 메뉴 개수 입력값을 파싱해서 HashMap 형태로 저장

--- src/main/java/utils/Parser.java
+++ src/main/java/utils/Parser.java
@@ -1,8 +1,30 @@
 package utils;
 
+import enums.Menu;
+
+import java.util.*;
+
 public class Parser {
 
+    private static final String COMMA = ",";
+    private static final String DELIMITER = "-";
     public static int convertToInt(String input) {
         return Integer.parseInt(input);
+    }
+
+    public static Map<String, Integer> extractMenuHashMap(String input){
+        Map<String, Integer> menuHashMap = new HashMap<>();
+        saveToHashMap(input, menuHashMap);
+        return menuHashMap;
+    }
+
+    private static void saveToHashMap(String input, Map<String, Integer> menuHashMap) {
+        String[] splitWithComma = input.split(COMMA);
+        for (String item : splitWithComma) {
+            String[] splitWithDelimiter = item.split(DELIMITER);
+            String menu = splitWithDelimiter[0].trim();
+            int count = convertToInt(splitWithDelimiter[1]);
+            menuHashMap.put(menu, count);
+        }
     }
 }
